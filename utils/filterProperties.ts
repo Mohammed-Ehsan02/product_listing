9792ca6a3bd498635750302d0876fe3f56c2f610
Mohammed Ehsan <mehsankhan02@gmail.com>
2025-02-08 15:38:55 +0400

    clean up

--- utils/filterProperties.ts
+++
@@ -1,9 +0,0 @@
-import { Property } from "@/types/property";
-
-export function filterProperties(properties: Property[], filters: { price?: number; bedrooms?: number }) {
-  return properties.filter((property) => {
-    const matchesPrice = filters.price !== undefined ? property.price <= filters.price : true;
-    const matchesBedrooms = filters.bedrooms !== undefined ? property.bedrooms === filters.bedrooms : true;
-    return matchesPrice && matchesBedrooms;
-  });
-}
